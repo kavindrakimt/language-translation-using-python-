208d77fd0c24689ba76ab6e69ec9deb3d4bebc68
John Bauer <horatio@gmail.com>
2021-03-23 14:15:34 -0700

    This should be a little faster, but need to untangle the tensors

--- stanza/models/tokenization/torch_data.py
+++ stanza/models/tokenization/torch_data.py
@@ -1,18 +1,27 @@
 from torch.utils.data import Dataset
 from stanza.models.tokenization.data import DataLoader
 import torch
+import numpy as np
+
+def condense_units(sentence):
+    units = [w[0] for w in sentence]
+    if all(len(unit) == 1 for unit in units):
+        return "".join(units)
+    else:
+        return units
 
 
 def condense_data(data):
-    new_data = []
-    for sentence in data:
-        units = [w[0] for w in sentence]
-        if all(len(unit) == 1 for unit in units):
-            new_data.append("".join(units))
-        else:
-            new_data.append(units)
+    new_data = [condense_units(sentence) for sentence in data]
     return new_data
 
+def condense_sentences(sentences):
+    #new_sentences = [[(np.array(sentence[0][0]),
+    #                   np.array(sentence[0][1]),
+    #                   np.array(sentence[0][2]),
+    #                   sentence[0][3])] for sentence in sentences]
+    #return new_sentences
+    return sentences
 
 class TokenizerDataset(Dataset):
     def __init__(self, docs, config, vocab):
@@ -30,5 +39,6 @@ class TokenizerDataset(Dataset):
 
         loader = DataLoader(self.config, input_text=text, vocab=self.vocab, evaluation=True)
         data = condense_data(loader.data)
-        return data, loader.sentences
+        sentences = condense_sentences(loader.sentences)
+        return data, sentences
 
